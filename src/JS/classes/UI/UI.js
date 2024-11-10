5dffc4cd13a8d1931f06b6bf9669194ab9fece8d
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-11-10 18:23:05 +0600

    Progress input validation is completed

--- src/JS/classes/UI/UI.js
+++ src/JS/classes/UI/UI.js
@@ -5,8 +5,54 @@ class UI {
     const addTaskBtn = selectElm(".add-task-btn");
     const taskModalContainer = selectElm(".task-modal-section");
     const modalCloseIcon = selectElm(".task-modal .close-icon");
-    return { addTaskBtn, taskModalContainer, modalCloseIcon };
+
+    const taskTitleInput = selectElm("#task-title");
+    const taskDescriptionInput = selectElm("#task-description");
+    const teamNameInput = selectElm("#team-name");
+    const deadLineInput = selectElm("#deadline");
+    const progressInput = selectElm("#progress");
+    const createTaskBtn = selectElm(".create-task");
+
+    return {
+      addTaskBtn,
+      taskModalContainer,
+      modalCloseIcon,
+      taskTitleInput,
+      taskDescriptionInput,
+      teamNameInput,
+      deadLineInput,
+      progressInput,
+      createTaskBtn,
+    };
   }
+
+  #handleProgressValue(e) {
+    console.log(e.target.value);
+    const progressInput = e.target;
+    const value = Number(progressInput.value);
+
+    if (value < 0) {
+      progressInput.value = 0;
+    }
+
+    if (value > 100) {
+      progressInput.value = 100;
+    }
+  }
+
+  #handleAddNewTask() {
+    console.log("add new task");
+    const {
+      taskTitleInput,
+      taskDescriptionInput,
+      teamNameInput,
+      deadLineInput,
+      progressInput,
+      createTaskBtn,
+    } = this.#loadSelector();
+  }
+  #handleEditTask() {}
+  #handleDeleteTask() {}
 
   #handleOpenModal(e) {
     const { taskModalContainer } = this.#loadSelector();
@@ -14,6 +60,15 @@ class UI {
     addStyle(taskModalContainer, { display: "flex" });
     console.log(e.currentTarget);
     const action = e.currentTarget.dataset.action;
+
+    switch (action) {
+      case "add":
+        this.#handleAddNewTask();
+        break;
+
+      default:
+        break;
+    }
   }
 
   #handleCloseModal(e) {
@@ -23,11 +78,13 @@ class UI {
   }
 
   init() {
-    const { addTaskBtn, taskModalContainer, modalCloseIcon } =
+    const { addTaskBtn, taskModalContainer, modalCloseIcon, progressInput } =
       this.#loadSelector();
 
     listenEvent(addTaskBtn, "click", this.#handleOpenModal.bind(this));
     listenEvent(modalCloseIcon, "click", this.#handleCloseModal.bind(this));
+
+    listenEvent(progressInput, "keyup", this.#handleProgressValue.bind(this));
   }
 }
 
