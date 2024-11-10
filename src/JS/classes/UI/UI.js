ad77dbf4649fa1b9c1bf73bc01734f753ad603af
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-11-10 19:04:39 +0600

    Deadline and progress input validation and hide input error message functionalities are completed

--- src/JS/classes/UI/UI.js
+++ src/JS/classes/UI/UI.js
@@ -6,12 +6,14 @@ class UI {
     const taskModalContainer = selectElm(".task-modal-section");
     const modalCloseIcon = selectElm(".task-modal .close-icon");
 
+    const taskModalBody = selectElm(".task-modal-body");
     const taskTitleInput = selectElm("#task-title");
     const taskDescriptionInput = selectElm("#task-description");
     const teamNameInput = selectElm("#team-name");
     const deadLineInput = selectElm("#deadline");
     const progressInput = selectElm("#progress");
-    const createTaskBtn = selectElm(".create-task");
+    // const createTaskBtn = selectElm(".create-task");
+    const modalActionsContainer = selectElm(".modal-actions");
 
     return {
       addTaskBtn,
@@ -22,8 +24,77 @@ class UI {
       teamNameInput,
       deadLineInput,
       progressInput,
-      createTaskBtn,
+      modalActionsContainer,
+      taskModalBody,
     };
+  }
+
+  #handleHideDeadlineErrMsg(e) {
+    console.log(e.target, "handleHidedeadlei");
+    console.log(e.target.value);
+    if (e.target.value) {
+      this.#hideInputRequiredMsg(e.target);
+    }
+  }
+
+  #handleInputChangeAndHideErrMsg(e) {
+    console.log(e.target);
+    const targetElm = e.target;
+    const tagName = targetElm.tagName.toLowerCase();
+    console.log(tagName);
+    if (tagName === "input" || tagName === "textarea") {
+      console.log(targetElm.type);
+      if (targetElm.value && targetElm.type !== "number") {
+        this.#hideInputRequiredMsg(targetElm);
+      }
+    }
+  }
+
+  #hideInputRequiredMsg(targetElm) {
+    addStyle(targetElm.nextElementSibling, { display: "none" });
+  }
+
+  #displayInputRequiredMsg(input) {
+    input.focus();
+    const errMsgElm = input.nextElementSibling;
+    addStyle(errMsgElm, { display: "block" });
+  }
+
+  #isInputFieldEmpty(input) {
+    return Number(input.value) === 0 ? true : false;
+  }
+
+  #inputValidation() {
+    const {
+      taskTitleInput,
+      taskDescriptionInput,
+      teamNameInput,
+      deadLineInput,
+      progressInput,
+    } = this.#loadSelector();
+
+    const isTitleEmpty = this.#isInputFieldEmpty(taskTitleInput);
+    if (isTitleEmpty) {
+      this.#displayInputRequiredMsg(taskTitleInput);
+      return;
+    }
+    const isDescriptionEmpty = this.#isInputFieldEmpty(taskDescriptionInput);
+    if (isDescriptionEmpty) {
+      this.#displayInputRequiredMsg(taskDescriptionInput);
+      return;
+    }
+    const isTeamNameEmpty = this.#isInputFieldEmpty(teamNameInput);
+    if (isTeamNameEmpty) {
+      this.#displayInputRequiredMsg(teamNameInput);
+      return;
+    }
+    const isDeadLineEmpty = this.#isInputFieldEmpty(deadLineInput);
+    if (isDeadLineEmpty) {
+      this.#displayInputRequiredMsg(deadLineInput);
+      return;
+    }
+
+    return true;
   }
 
   #handleProgressValue(e) {
@@ -48,27 +119,33 @@ class UI {
       teamNameInput,
       deadLineInput,
       progressInput,
-      createTaskBtn,
     } = this.#loadSelector();
+
+    const isSuccessValidation = this.#inputValidation();
+
+    if (!isSuccessValidation) return;
   }
   #handleEditTask() {}
   #handleDeleteTask() {}
 
-  #handleOpenModal(e) {
-    const { taskModalContainer } = this.#loadSelector();
-
-    addStyle(taskModalContainer, { display: "flex" });
-    console.log(e.currentTarget);
-    const action = e.currentTarget.dataset.action;
+  #handleModalActions(e) {
+    const action = e.target.dataset.action;
+    console.log(action);
 
     switch (action) {
-      case "add":
+      case "create":
         this.#handleAddNewTask();
         break;
 
       default:
         break;
     }
+  }
+
+  #handleOpenModal(e) {
+    const { taskModalContainer } = this.#loadSelector();
+
+    addStyle(taskModalContainer, { display: "flex" });
   }
 
   #handleCloseModal(e) {
@@ -78,13 +155,43 @@ class UI {
   }
 
   init() {
-    const { addTaskBtn, taskModalContainer, modalCloseIcon, progressInput } =
-      this.#loadSelector();
+    const {
+      addTaskBtn,
+      taskModalContainer,
+      modalCloseIcon,
+      progressInput,
+      modalActionsContainer,
+      taskModalBody,
+      deadLineInput,
+    } = this.#loadSelector();
 
     listenEvent(addTaskBtn, "click", this.#handleOpenModal.bind(this));
+
     listenEvent(modalCloseIcon, "click", this.#handleCloseModal.bind(this));
 
     listenEvent(progressInput, "keyup", this.#handleProgressValue.bind(this));
+    listenEvent(
+      deadLineInput,
+      "change",
+      this.#handleHideDeadlineErrMsg.bind(this)
+    );
+
+    listenEvent(
+      taskModalBody,
+      "keyup",
+      this.#handleInputChangeAndHideErrMsg.bind(this)
+    );
+
+    listenEvent(
+      modalActionsContainer,
+      "click",
+      this.#handleModalActions.bind(this)
+    );
+    listenEvent(
+      modalActionsContainer,
+      "click",
+      this.#handleModalActions.bind(this)
+    );
   }
 }
 
