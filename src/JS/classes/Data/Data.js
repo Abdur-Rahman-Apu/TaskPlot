ad38e27c07a4e95b9166db3b79b35b19b5e3309c
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-11-12 16:27:09 +0600

    Comments are added into the data.js file

--- src/JS/classes/Data/Data.js
+++ src/JS/classes/Data/Data.js
@@ -2,21 +2,31 @@ class Data {
   #allTasks = [];
   #displayTasks;
 
+  // allTasks variable holds all tasks data
+
+  // two type data can be received array or single object
   set allTasks(data) {
     this.#allTasks = Array.isArray(data)
       ? [...this.#allTasks, ...data]
       : [...this.#allTasks, data];
   }
 
-  set emptyAllTasks(data) {
-    this.#allTasks = data;
-  }
+  // get all tasks
   get allTasks() {
     return this.#allTasks;
   }
+
+  // set allTask empty
+  set emptyAllTasks(data) {
+    this.#allTasks = data;
+  }
+
+  // displayTasks variable holds data which need to be displayed into the UI
+
   set displayTasks(data) {
     this.#displayTasks = data;
   }
+
   get displayTasks() {
     return this.#displayTasks;
   }
