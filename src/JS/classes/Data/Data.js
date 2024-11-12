34dd28dd3d48e136373931db13482d2da5eaa238
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-11-12 16:28:23 +0600

    Comments are added into the Storage.js file and Data.js file

--- src/JS/classes/Data/Data.js
+++ src/JS/classes/Data/Data.js
@@ -1,4 +1,5 @@
 class Data {
+  // private variables
   #allTasks = [];
   #displayTasks;
 
--- src/JS/classes/Storage/Storage.js
+++ src/JS/classes/Storage/Storage.js
@@ -1,8 +1,10 @@
 class Storage {
+  // get data from the localStorage
   getFromStorage() {
     return JSON.parse(localStorage.getItem("TaskPlot"));
   }
 
+  // set data into the localStorage
   setIntoStorage(data) {
     localStorage.setItem("TaskPlot", JSON.stringify(data));
   }
